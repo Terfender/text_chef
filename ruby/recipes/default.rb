fd65ddda47d68fa568be4767c8c5381a5411b2c2
farhan <farhan@Farhans-MacBook-Pro.local>
2018-01-14 18:06:06 -0500

    test1

--- ruby/recipes/default.rb
+++ ruby/recipes/default.rb
@@ -1,25 +1,39 @@
 # Install rbenv and ruby
 bash "Install rbenv and ruby" do
   code <<-EOF
-  mkdir -p /home/deploy/enterd
   echo 'hellooooooo'
-  mkdir -p /home/deploy/echo
-  #su deploy
-  mkdir -p /home/deploy/su
+  su deploy
 
   curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-  wget https://deb.nodesource.com/setup_8.x
-  mkdir -p /home/deploy/curl1
   curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-  wget https://dl.yarnpkg.com/debian/pubkey.gpg
-  mkdir -p /home/deploy/curl2
   echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-  mkdir -p /home/deploy/echo2
 
   sudo apt-get update
-  mkdir -p /home/deploy/update
   sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev nodejs yarn
-  mkdir -p /home/deploy/apt_get
+
+  cd
+  git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+  echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+  echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+  exec $SHELL
+
+  git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+  echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
+  exec $SHELL
+
+  rbenv install 2.5.0
+  rbenv global 2.5.0
+  ruby -v
+
+  gem install bundler
+
+
+  
+  curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+  echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+
+  sudo apt-get update
+  sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev nodejs yarn
 
   cd
   git clone https://github.com/rbenv/rbenv.git ~/.rbenv
@@ -44,21 +58,51 @@ bash "Install rbenv and ruby" do
 end
 
 
-
-execute "Install rbenv and ruby -- debug" do
-  # command "gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3"
-  command "echo 'hellooooooo2'"
-  user "root"
-end
-
-bash "create test folder 1" do
-  code "mkdir -p /home/deploy/folder1"
+# Install RVM
+execute "Installing pubkey.gpg" do
+  command "curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -"
   user "deploy"
 end
 
-bash "create test folder 2" do
+# Install rbenv
+execute "Installing rbenv and Ruby" do
+  command "curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -"
+  user "deploy"
+end
+
+# Install basic packages
+%w(git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev nodejs yarn).each do |pkg|
+  apt_package pkg
+end
+
+# git
+execute "git rbenv" do
+  command "git clone https://github.com/rbenv/rbenv.git ~/.rbenv"
+  user "deploy"
+end
+bash "bashrc" do
   code <<-EOF
-    mkdir -p /home/deploy/folder2
+    echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
   EOF
   user "deploy"
 end
+bash "rbenv eval bashrc" do
+  code <<-EOF
+    echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+  EOF
+  user "deploy"
+end
+execute "exec $SHELL" do
+  command "exec $SHELL"
+  user "deploy"
+end
+execute "rbenv install 2.5.0" do
+  command "rbenv install 2.5.0"
+  user "deploy"
+end
+execute "rbenv global 2.5.0" do
+  command "rbenv global 2.5.0"
+  user "deploy"
+end
+
+
