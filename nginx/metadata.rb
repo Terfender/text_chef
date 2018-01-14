ece9d7d81d19463299aaeb5ff19aef13bb408324
farhan <farhan@Farhans-MacBook-Pro.local>
2018-01-14 16:21:20 -0500

    added metadata

---
+++ nginx/metadata.rb
@@ -0,0 +1,4 @@
+name        "nginx"
+description "Installas nginx"
+maintainer  "AdStash"
+version     "1.0.0"
---
+++ ubuntu/metadata.rb
@@ -0,0 +1,4 @@
+name        "Ubuntu"
+description "Updates Ubuntu"
+maintainer  "AdStash"
+version     "1.0.0"
