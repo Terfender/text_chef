dbb3b4ef326414dacba1a9eeaf09130865f4acda
farhan <farhan@Farhans-MacBook-Pro.local>
2018-01-15 00:04:29 -0500

    dir test

--- ruby/recipes/default.rb
+++ ruby/recipes/default.rb
@@ -2,8 +2,9 @@
 bash "Install rbenv and ruby" do
   code <<-EOF
     su ubuntu
-
+    mkdir ~/hello
     curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+    mkdir ~/curl
     curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
     echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
