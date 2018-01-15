d48a8cab0145755d71b642ce7c3fd60e281ec3ff
farhan <farhan@Farhans-MacBook-Pro.local>
2018-01-14 21:12:25 -0500

    fixed dir

--- ruby/recipes/default.rb
+++ ruby/recipes/default.rb
@@ -10,14 +10,14 @@ bash "Install rbenv and ruby" do
     sudo apt-get update
     sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev nodejs yarn
 
-    cd ~/home/deploy
-    git clone https://github.com/rbenv/rbenv.git ~/.rbenv
-    echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
-    echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+    cd /home/deploy
+    git clone https://github.com/rbenv/rbenv.git /home/deploy/.rbenv
+    echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> /home/deploy/.bashrc
+    echo 'eval "$(rbenv init -)"' >> /home/deploy/.bashrc
     exec $SHELL
 
-    git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
-    echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
+    git clone https://github.com/rbenv/ruby-build.git /home/deploy/.rbenv/plugins/ruby-build
+    echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> /home/deploy/.bashrc
     exec $SHELL
 
     rbenv install 2.5.0
