796b13436e4a0ade4ff1811683efd4b799f4bbfd
farhan <farhan@Farhans-MacBook-Pro.local>
2018-01-14 21:39:27 -0500

    test with password

--- ruby/recipes/default.rb
+++ ruby/recipes/default.rb
@@ -1,7 +1,8 @@
 # Install rbenv and ruby
 bash "Install rbenv and ruby" do
   code <<-EOF
-    # su deploy
+    su deploy
+    echo 12121212 | sudo -S mkdir /home/temp
 
     curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
     curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
@@ -10,14 +11,16 @@ bash "Install rbenv and ruby" do
     sudo apt-get update
     sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev nodejs yarn
 
-    cd /home/deploy
-    git clone https://github.com/rbenv/rbenv.git /home/deploy/.rbenv
-    echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> /home/deploy/.bashrc
-    echo 'eval "$(rbenv init -)"' >> /home/deploy/.bashrc
+
+
+    cd
+    git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+    echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+    echo 'eval "$(rbenv init -)"' >> ~/.bashrc
     exec $SHELL
 
-    git clone https://github.com/rbenv/ruby-build.git /home/deploy/.rbenv/plugins/ruby-build
-    echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> /home/deploy/.bashrc
+    git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+    echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
     exec $SHELL
 
     rbenv install 2.5.0
