d75e655f722f835de3ba84aaaf239c7658b16221
farhan <farhan@Farhans-MacBook-Pro.local>
2018-01-16 13:38:08 -0500

    test

--- ruby/recipes/default.rb
+++ ruby/recipes/default.rb
@@ -12,7 +12,8 @@ bash "Install rbenv and ruby" do
 
     sudo apt-get update
     sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev nodejs yarn
-
+    sudo apt-get install -y libssl-dev libreadline-dev zlib1g-dev
+    
     cd /home/ubuntu
 
     if [ -d /home/ubuntu/.rbenv ]
