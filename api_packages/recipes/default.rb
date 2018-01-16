b4978a63df7027ea1d7da88d97bb105ed0241970
farhan <farhan@Farhans-MacBook-Pro.local>
2018-01-16 17:45:50 -0500

    yes all

--- api_packages/recipes/default.rb
+++ api_packages/recipes/default.rb
@@ -2,22 +2,23 @@
 bash "Install rbenv and ruby" do
   code <<-EOF
     sudo apt-get install -y libav-tools
-    
+
     sudo apt-get install -y libmagickcore-dev libmagickwand-dev libmagic-dev
 
     sudo apt-get install -y build-essential checkinstall && apt-get build-dep imagemagick
 
     cd /home/ubuntu
-    sudo wget http://www.imagemagick.org/download/ImageMagick.tar.gz
+    rm -rf ImageMagick*
     mkdir ImageMagick
+    sudo wget http://www.imagemagick.org/download/ImageMagick.tar.gz
     sudo tar xzvf ImageMagick.tar.gz -C /home/ubuntu/ImageMagick --strip-components=1
 
     cd ImageMagick
-    ./configure
+    sudo ./configure
     make clean
     make
     checkinstall
-    ldconfig /usr/local/lib
+    sudo sh -c "ldconfig /usr/local/lib"
     magick -version
 
   EOF
