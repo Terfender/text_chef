e65909ca935de6108c71b48c68860a1c7e285f61
farhan <farhan@Farhans-MacBook-Pro.local>
2018-01-16 16:56:18 -0500

    nginx

--- nginx/recipes/default.rb
+++ nginx/recipes/default.rb
@@ -2,19 +2,27 @@
 bash "Installing nginx" do
   code <<-EOF
 
-  su deploy
+    sudo apt-get install -y dirmngr gnupg
+    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 561F9B9CAC40B2F7
+    sudo apt-get install -y apt-transport-https ca-certificates
 
-  sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 561F9B9CAC40B2F7
-  sudo apt-get install -y apt-transport-https ca-certificates
+    # Add our APT repository
+    sudo sh -c 'echo deb https://oss-binaries.phusionpassenger.com/apt/passenger xenial main > /etc/apt/sources.list.d/passenger.list'
+    sudo apt-get update
 
-  sudo sh -c 'echo deb https://oss-binaries.phusionpassenger.com/apt/passenger xenial main > /etc/apt/sources.list.d/passenger.list'
-  sudo apt-get update
-
-  sudo apt-get install -y nginx-extras passenger
-  
-  sudo service nginx start
-
+    # Install Passenger + Nginx
+    sudo apt-get install -y nginx-extras passenger
   EOF
   user "root"
 
+end
+
+
+
+cookbook_file "Copy nginx.conf" do  
+  group "root"
+  mode "0644"
+  owner "root"
+  path "/etc/nginx/nginx.conf"
+  source "nginx.conf"  
 end
