1bf4fe0ecbd792f2b1046f390c725808c13ca010
farhan <farhan@Farhans-MacBook-Pro.local>
2018-01-22 12:34:58 -0500

    test not to install postgres

--- api_packages/recipes/default.rb
+++ api_packages/recipes/default.rb
@@ -3,8 +3,6 @@ bash "Install API packages" do
   code <<-EOF
     sudo apt-get install -y libav-tools libmagickcore-dev libmagickwand-dev libmagic-dev libpq-dev
     sudo apt-get install -y build-essential checkinstall && apt-get build-dep imagemagick
-    sudo apt-get install -y postgresql postgresql-contrib
-
 
     cd /home/ubuntu
     rm -rf ImageMagick*
