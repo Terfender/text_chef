98e79ee63c888e22dc850d7a3ab03eea8da5e320
farhan <farhan@Farhans-MacBook-Pro.local>
2018-01-22 12:43:58 -0500

    added setup recipe

---
+++ setup/metadata.rb
@@ -0,0 +1,10 @@
+name        "nginx"
+description "Installas nginx"
+maintainer  "AdStash"
+version     "1.0.0"
+
+depends 'ubuntu_upgrade'
+depends 'nginx'
+depends 'ruby'
+depends 'api_packages'
+depends 'redis'
---
+++ setup/recipes/default.rb
@@ -0,0 +1,6 @@
+# trigger setup recipes
+include_recipe 'ubuntu_upgrade'
+include_recipe 'nginx'
+include_recipe 'ruby'
+include_recipe 'api_packages'
+include_recipe 'redis'
