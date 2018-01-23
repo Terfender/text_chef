eb20743c7608577128b54e0a7c0bad368542fc44
farhan <farhan@Farhans-MacBook-Pro.local>
2018-01-23 11:55:14 -0500

    added seed

---
+++ seed/metadata.rb
@@ -0,0 +1,4 @@
+name        "Seed"
+description "Runs DB seed"
+maintainer  "AdStash"
+version     "1.0.0"
---
+++ seed/recipes/default.rb
@@ -0,0 +1,25 @@
+# Initializers
+deploy_root       = '/home/ubuntu/apps'
+app               = search("aws_opsworks_app").first
+app_name          = app['name'] #'AdSatash'
+current_release   = "#{deploy_root}/#{app_name}/current"
+instances         = search("aws_opsworks_instance")
+online_instances  = instances.select{ |instance| instance['status'] == 'online'}
+instance          = instances.sort_by{ |instance| instance['instance_id'] }.first
+bundle_path       = '/home/ubuntu/.rbenv/shims/bundle'
+
+
+
+if instance['self']
+  Chef::Log.warn('This instance will run DB seed')
+  bash "run migration" do
+    cwd current_release
+    code <<-EOF
+      '#{bundle_path}' exec rake db:seed RAILS_ENV=production
+    EOF
+    user "ubuntu"
+    environment ({'HOME' => '/home/ubuntu'})
+  end
+else
+  Chef::Log.warn('This instance will NOT run DB seed')
+end
