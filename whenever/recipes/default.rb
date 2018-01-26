2a02258f94c79bd9328dc41d45728961da8c7f71
Fernando Gorodscy <fegorodscy@gmail.com>
2018-01-26 17:40:16 -0500

    Add recipe for whenver

---
+++ whenever/recipes/default.rb
@@ -0,0 +1,25 @@
+# Initializers
+deploy_root       = '/home/ubuntu/apps'
+app               = search("aws_opsworks_app").first
+app_name          = app['name'] #'AdSatash'
+current_release   = "#{deploy_root}/#{app_name}/current"
+instances         = search("aws_opsworks_instance")
+online_instances  = instances.select{ |instance| instance['status'] == 'online'}
+instance          = instances.sort_by{ |instance| instance['instance_id'] }.first
+bundle_path       = '/home/ubuntu/.rbenv/shims/bundle'
+
+
+
+if instance['self']
+  Chef::Log.warn('This instance will run Whenever')
+  bash "run whenver" do
+    cwd current_release
+    code <<-EOF
+      '#{bundle_path}' exec whenever --update-crontab --set environment='production'
+    EOF
+    user "ubuntu"
+    environment ({'HOME' => '/home/ubuntu'})
+  end
+else
+  Chef::Log.warn('This instance will NOT run Whenever')
+end
