5d0fe887866c16d3adae661bb370b2fc6c445eee
Fernando Gorodscy <fegorodscy@gmail.com>
2018-01-29 14:02:00 -0500

    Update whenever recipe to run whenever always on current release

--- whenever/recipes/default.rb
+++ whenever/recipes/default.rb
@@ -1,7 +1,7 @@
 # Initializers
 deploy_root       = '/home/ubuntu/apps'
 app               = search("aws_opsworks_app").first
-app_name          = app['name'] #'AdSatash'
+app_name          = app['name'] #'AdStash'
 current_release   = "#{deploy_root}/#{app_name}/current"
 instances         = search("aws_opsworks_instance")
 online_instances  = instances.select{ |instance| instance['status'] == 'online'}
@@ -14,8 +14,8 @@ if instance['self']
   Chef::Log.warn('This instance will run Whenever')
   bash "run whenver" do
     cwd current_release
-    code <<-EOF
-      '#{bundle_path}' exec whenever --update-crontab --set environment='production'
+    code <<~EOF
+      '#{bundle_path}' exec whenever --update-crontab --set environment='production' --set path='#{current_release}'
     EOF
     user "ubuntu"
     environment ({'HOME' => '/home/ubuntu'})
